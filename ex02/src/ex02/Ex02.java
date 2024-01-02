ea65a9df3e3d0461e8f8336951b8099376f2886b
yayoung92 <140130695+yayoung92@users.noreply.github.com>
2024-01-02 11:09:57 +0900

    24.1.2

---
+++ ex02/src/ex02/Ex02.java
@@ -0,0 +1,21 @@
+package ex02;
+
+public class Ex02 {
+	// ctrl + shift + f 자동정렬됨. 맨 앞에 탭 누른 효과
+	// shift + 탭 맨앞 빈 부분 없이 맨 앞으로 땡겨짐.
+	public static void doA() {
+		String a = "abcdefd";
+		System.out.println(a);
+	}
+
+	public static void main(String[] agrs) {
+		String a = "abc";
+		String b = "def";
+
+		System.out.println(a);
+		System.out.println(b);
+		
+		doA();
+	}
+
+}
