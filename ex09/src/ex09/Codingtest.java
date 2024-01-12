47295c9017572f0e9b234fbf969bfbfd909d44e3
yayoung92 <yangga0744@gmail.com>
2024-01-12 20:45:45 +0900

    24.1.12

---
+++ ex09/src/ex09/Codingtest.java
@@ -0,0 +1,17 @@
+package ex09;
+
+public class Codingtest {
+	public static void main(String[] args) {
+		int[] arr = {0,1,2,4,3};
+		int[][] queries = {
+				{0,4,2},  // 0
+				{0,3,2},  // 1
+				{0,2,2}   // 2
+		};
+		int min = 0;
+		
+		for(int i=0; i<queries.length; i++) {
+			int m = queries[i][0];
+		}
+	}
+}
